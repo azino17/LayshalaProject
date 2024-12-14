84801e556c5aa60f58bf6e0823558561ede75179
unknown <aayushkhambayate123@gmail.com>
2024-12-14 09:21:33 +0530

    second commit

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -1,41 +1,63 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faTwitter, faInstagram , faLinkedin} from '@fortawesome/free-brands-svg-icons';
-
-
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faTwitter,
+  faInstagram,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
+  const navigate = useNavigate();
+
+  const handleClickStudent = () => {
+    alert("Please register first.");
+    navigate("/register");
+  };
+
+  const handleClickAdmin = () => {
+    alert("Please login first.");
+    navigate("/login");
+  };
+
   return (
     <>
       {/* Footer */}
-      <footer className="text-center text-white text-lg-start text-muted" style={{ backgroundColor: '#B8860B', color: 'white', padding: '20px 0' }}>
+      <footer
+        className="text-center text-white text-lg-start text-muted"
+        style={{
+          backgroundColor: "#B8860B",
+          color: "white",
+          padding: "20px 0",
+        }}
+      >
         {/* Section: Social media */}
         <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
           {/* Left */}
-          <div className="me-5 d-none d-lg-block">
+          <div className="me-5 d-none d-lg-block" style={{color:"white",fontFamily:"Noto Sans"}}>
             <span>Get connected with us on social networks:</span>
           </div>
           {/* Left */}
 
           {/* Right */}
           <div>
-            <Link href="" className="me-4 text-reset">
+            <Link to="{}" className="me-4 text-reset">
               <i className="fab fa-facebook-f"></i>
             </Link>
-            <Link href="" className="me-4 text-reset">
+            <Link to="" className="me-4 text-reset">
               <i className="fab fa-twitter"></i>
             </Link>
-            <Link href="" className="me-4 text-reset">
+            <Link to="" className="me-4 text-reset">
               <i className="fab fa-google"></i>
             </Link>
-            <Link href="" className="me-4 text-reset">
+            <Link to="" className="me-4 text-reset">
               <i className="fab fa-instagram"></i>
             </Link>
-            <Link href="" className="me-4 text-reset">
+            <Link to="" className="me-4 text-reset">
               <i className="fab fa-linkedin"></i>
             </Link>
-            <Link href="" className="me-4 text-reset">
+            <Link to="" className="me-4 text-reset">
               <i className="fab fa-github"></i>
             </Link>
           </div>
@@ -51,66 +73,157 @@ const Footer = () => {
               {/* Grid column */}
               <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
                 {/* Content */}
-                <h6 className="text-uppercase fw-bold mb-4 text-white" >
+                <h6 className="text-uppercase fw-bold mb-4 text-white" style={{fontFamily:"Noto Sans"}}>
                   <i className="fas fa-gem me-3"></i>Company name
                 </h6>
-                <p className='text-white' style={{textAlign:'justify'}}>
-                  Here you can use rows and columns to organize your footer content. Lorem ipsum
-                  dolor sit amet, consectetur adipisicing elit.
+                <p className="text-white" style={{ textAlign: "justify",fontFamily:"Noto Sans"}}>
+                  Here you can use rows and columns to organize your footer
+                  content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                  elit.
                 </p>
               </div>
-              {/* Grid column */}
+            
 
               {/* Grid column */}
-              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4" style={{ fontFamily: 'Lora, serif',color:'#FFFFFF'}}>
+              <div
+                className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4"
+                style={{ fontFamily: "Noto Sans", color: "#FFFFFF" }}
+              >
                 {/* Links */}
-                <h6 className="fw-bold mb-4">
-                  Students
-                </h6>
+                <h6 className="fw-bold mb-4">Students</h6>
                 <p>
-                  <Link href="#!" className="text-reset"  style={{textDecoration:'none'}}>Student's Login</Link>
+                  <Link
+                    to="/login/student"
+                    className="text-reset"
+                    style={{ textDecoration: "none" }}
+                  >
+                    Student's Login
+                  </Link>
                 </p>
                 <p>
-                  <Link href="#!" className="text-reset"  style={{textDecoration:'none'}}>Student's Registration</Link>
+                  <Link
+                    to="/register"
+                    className="text-reset"
+                    style={{ textDecoration: "none" }}
+                  >
+                    Student's Registration
+                  </Link>
                 </p>
                 <p>
-                  <Link href="#!" className="text-reset"  style={{textDecoration:'none'}}>Courses</Link>
+                  <span
+                    className="text-reset"
+                    onClick={handleClickStudent}
+                  >
+                    <Link
+                      to="#!"
+                      className="text-reset"
+                      style={{ textDecoration: "none" }}
+                    >
+                      Courses
+                    </Link>
+                  </span>
                 </p>
                 <p>
-                  <Link href="#!" className="text-reset"  style={{textDecoration:'none'}}>Online Materials</Link>
+                <span
+                    className="text-reset"
+                    onClick={handleClickStudent}
+                  >
+                  <Link
+                    to="#!"
+                    className="text-reset"
+                    style={{ textDecoration: "none" }}
+                  >
+                    Online Materials
+                  </Link>
+                  </span>
                 </p>
               </div>
               {/* Grid column */}
 
               {/* Grid column */}
-              <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4" style={{ fontFamily: 'Lora, serif',color:'#FFFFFF'}}>
+              <div
+                className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4"
+                style={{ fontFamily: "Noto Sans", color: "#FFFFFF" }}
+              >
                 {/* Links */}
-                <h6 className="text-uppercase fw-bold mb-4">
-                  Admins
-                </h6>
+                <h6 className="text-uppercase fw-bold mb-4">Admins</h6>
                 <p>
-                  <Link href="#!" className="text-reset"  style={{textDecoration:'none'}}>Admin's Login</Link>
+                  <Link
+                    to="/login"
+                    className="text-reset"
+                    style={{ textDecoration: "none" }}
+                  >
+                    Admin's Login
+                  </Link>
                 </p>
                 <p>
-                  <Link href="#!" className="text-reset"  style={{textDecoration:'none'}}>Admin's Registration</Link>
+                <span
+                    className="text-reset"
+                    onClick={handleClickAdmin}
+                  >
+                  <Link
+                    to="#!"
+                    className="text-reset"
+                    style={{ textDecoration: "none" }}
+                  >
+                    Admin's Registration
+                  </Link>
+                  </span>
                 </p>
                 <p>
-                  <Link href="#!" className="text-reset"  style={{textDecoration:'none'}}>Courses</Link>
+                <span
+                    className="text-reset"
+                    onClick={handleClickAdmin}
+                  >
+                  <Link
+                    to="#!"
+                    className="text-reset"
+                    style={{ textDecoration: "none" }}
+                  >
+                    Courses
+                  </Link>
+                  </span>
                 </p>
                 <p>
-                  <Link href="#!" className="text-reset"  style={{textDecoration:'none'}}>Online Materials</Link>
+                <span
+                    className="text-reset"
+                    onClick={handleClickAdmin}
+                  >
+                  <Link
+                    to="#!"
+                    className="text-reset"
+                    style={{ textDecoration: "none" }}
+                  >
+                    Online Materials
+                  </Link>
+                  </span>
                 </p>
               </div>
-              {/* Grid column */}
+           
 
               {/* Grid column */}
-              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4" style={{ fontFamily: 'Lora, serif',color:'#FFFFFF'}}>
+              <div
+                className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4"
+                style={{ fontFamily: "Noto Sans", color: "#FFFFFF" }}
+              >
                 {/* Links */}
                 <h6 className="text-uppercase fw-bold mb-4">Follow Us</h6>
-                <p> <FontAwesomeIcon icon={faInstagram}/> layshalafoundation</p>
-                <p> <FontAwesomeIcon icon={faTwitter} /> layshalafoundation</p>
-                <p> <FontAwesomeIcon icon={faFacebookF} />  layshalafoundation</p>
-                <p> <FontAwesomeIcon icon={faLinkedin} /> layshalafoundation</p>
+                <p>
+                  {" "}
+                  <FontAwesomeIcon icon={faInstagram} /> layshalafoundation
+                </p>
+                <p>
+                  {" "}
+                  <FontAwesomeIcon icon={faTwitter} /> layshalafoundation
+                </p>
+                <p>
+                  {" "}
+                  <FontAwesomeIcon icon={faFacebookF} /> layshalafoundation
+                </p>
+                <p>
+                  {" "}
+                  <FontAwesomeIcon icon={faLinkedin} /> layshalafoundation
+                </p>
               </div>
               {/* Grid column */}
             </div>
@@ -120,15 +233,20 @@ const Footer = () => {
         {/* Section: Links  */}
 
         {/* Copyright */}
-        <div className="text-center p-4" style={{backgroundColor: 'rgba(0, 0, 0, 0.05)'}}>
-          © 2021 Copyright:
-          <Link className="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</Link>
+        <div
+          className="text-center p-4"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
+        >
+          © 2024 Copyright: All rights reserved &nbsp;
+          <Link className="text-reset fw-bold" to="https://layshala.org/">
+            Layshala.org
+          </Link>
         </div>
         {/* Copyright */}
       </footer>
-      {/* Footer */} 
+      {/* Footer */}
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- front/src/pages/Adminpages/AdminViewAchievement.js
+++ front/src/pages/Adminpages/AdminViewAchievement.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import jsPDF from "jspdf";
-import html2canvas from "html2canvas";
 import { Table, Button, Modal, InputGroup, FormControl } from "react-bootstrap";
 
 const AdminViewAchievement = () => {
@@ -36,10 +35,10 @@ const AdminViewAchievement = () => {
     setShowModal(true); // Open the modal
   };
 
-  const handleCloseModal = () => {
-    setSelectedAchievement(null);
-    setShowModal(false);
-  };
+  // const handleCloseModal = () => {
+  //   setSelectedAchievement(null);
+  //   setShowModal(false);
+  // };
 
   // Filter achievements based on search query
   const filteredAchievements = achievements.filter((achievement) =>
@@ -80,7 +79,13 @@ const AdminViewAchievement = () => {
 
   return (
     <div className="container mt-4">
-      <h1 className="mb-4">Achievements</h1>
+     <div className="d-flex justify-content-between align-items-center mb-4">
+        <div>
+          <h4>Achievements</h4>
+          <p className="text-muted">
+          "Student Achievements: Celebrating Excellence and Success 🎓🏆"</p>
+        </div>
+      </div>
       {/* Search Bar */}
       <InputGroup className="mb-3">
         <FormControl
@@ -89,79 +94,124 @@ const AdminViewAchievement = () => {
           onChange={(e) => setSearchQuery(e.target.value)}
         />
       </InputGroup>
+
       {/* Achievement Table */}
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Student Name</th>
-            <th>Event Name</th>
-            <th>Event Date</th>
-            <th>Rank</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredAchievements.map((achievement, index) => (
-            <tr key={achievement._id}>
-              <td>{index + 1}</td>
-              <td>{achievement.studentId?.studentName || "N/A"}</td>
-              <td>{achievement.eventName}</td>
-              <td>{new Date(achievement.eventDate).toLocaleDateString()}</td>
-              <td>{achievement.rank}</td>
-              <td>
-                <Button
-                  variant="info"
-                  size="sm"
-                  onClick={() => handleShowModal(achievement)}
-                >
-                  View Certificate
-                </Button>
-              </td>
+      <div className="table-responsive">
+        <Table
+          striped
+          bordered
+          hover
+          className="text-center"
+          style={{ fontFamily: "'Noto Sans', sans-serif" }}
+        >
+          <thead className="bg-primary text-white">
+            <tr>
+              <th>ID</th>
+              <th>Student Name</th>
+              <th>Event Name</th>
+              <th>Event Date</th>
+              <th>Rank</th>
+              <th>Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {filteredAchievements.map((achievement, index) => {
+              const rowClass = achievement.rank === 1 ? "" : "";
+              return (
+                <tr key={achievement._id} className={rowClass}>
+                  <td>{index + 1}</td>
+                  <td>{achievement.studentId?.studentName || "N/A"}</td>
+                  <td>{achievement.eventName}</td>
+                  <td>
+                    {new Date(achievement.eventDate).toLocaleDateString()}
+                  </td>
+                  <td>{achievement.rank}</td>
+                  <td>
+                    <Button
+                      variant=""
+                      size="sm"
+                      onClick={() => handleShowModal(achievement)}
+                      className="btn w-100"
+                      style={{
+                        backgroundColor: "#FFA800",
+                        fontFamily: "'Noto Sans', sans-serif",
+                        color:"white"
+                      }}
+                      onMouseEnter={(e) => (e.target.style.color = "#800000")}
+                      onMouseLeave={(e) => (e.target.style.color = "white")}
+                    >
+                      <i className="bi bi-file-earmark-text me-1"></i> View
+                      Certificate
+                    </Button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
+
       {/* Modal for Viewing Certificate */}
-      <Modal show={showModal} onHide={() => setShowModal(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>Achievement Details</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {selectedAchievement && (
-            <div>
-              <p>
-                <strong>Student Name:</strong>{" "}
-                {selectedAchievement.studentId.studentName}
-              </p>
-              <p>
-                <strong>Event Name:</strong> {selectedAchievement.eventName}
-              </p>
-              <p>
-                <strong>Event Date:</strong>{" "}
-                {new Date(selectedAchievement.eventDate).toLocaleDateString()}
-              </p>
-              <p>
-                <strong>Rank:</strong> {selectedAchievement.rank}
-              </p>
-              <p>
-                <strong>Certificate:</strong>
-              </p>
-              <img
-                src={`data:image/jpeg;base64,${selectedAchievement.certificate}`}
-                alt="Certificate"
-                style={{ maxWidth: "100%", height: "auto" }}
-              />
-              <button
-                className="btn btn-primary mt-3"
-                onClick={() => downloadAchievementAsPDF(selectedAchievement)}
-              >
-                Download as PDF
-              </button>
-            </div>
-          )}
-        </Modal.Body>
-      </Modal>
+<Modal show={showModal} onHide={() => setShowModal(false)}>
+  <Modal.Header closeButton>
+    <Modal.Title>Achievement Details</Modal.Title>
+  </Modal.Header>
+  <Modal.Body
+    style={{ fontFamily: "'Noto Sans', sans-serif", textAlign: "left" }}
+  >
+    {selectedAchievement && (
+      <div>
+        <p>
+          <strong>Student Name:</strong> {selectedAchievement.studentId.studentName}
+        </p>
+        <p>
+          <strong>Event Name:</strong> {selectedAchievement.eventName}
+        </p>
+        <p>
+          <strong>Event Date:</strong>{" "}
+          {new Date(selectedAchievement.eventDate).toLocaleDateString()}
+        </p>
+        <p>
+          <strong>Rank:</strong> {selectedAchievement.rank}
+        </p>
+        <p>
+          <strong>Certificate:</strong>
+        </p>
+        <div style={{ textAlign: "center" }}>
+          <img
+            src={`data:image/jpeg;base64,${selectedAchievement.certificate}`}
+            alt="Certificate"
+            style={{
+              maxWidth: "90%", // Ensures the image is not too large
+              height: "auto",
+              borderRadius: "8px", // Optional for rounded corners
+              boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)", // Optional for a subtle shadow
+            }}
+          />
+        </div>
+        <div className="mt-3 text-center">
+          <button
+            style={{
+              backgroundColor: "#FFA800",
+              fontFamily: "'Noto Sans', sans-serif",
+              color: "white",
+              border: "none",
+              padding: "10px 20px",
+              borderRadius: "5px",
+              cursor: "pointer",
+            }}
+            onMouseEnter={(e) => (e.target.style.color = "#800000")}
+            onMouseLeave={(e) => (e.target.style.color = "white")}
+            onClick={() => downloadAchievementAsPDF(selectedAchievement)}
+          >
+            Download as PDF
+          </button>
+        </div>
+      </div>
+    )}
+  </Modal.Body>
+</Modal>
+
     </div>
   );
 };
