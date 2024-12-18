11544ab662cf0e1d6d4b9efbf8a2365494f511f3
unknown <aayushkhambayate123@gmail.com>
2024-12-18 11:15:48 +0530

    eight commit

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use("/api/admin", adminRoutes);
 
 // Connect to MongoDB
 mongoose
-    .connect('mongodb://127.0.0.1:27017/registration', { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('MongoDB connection error:', err));
 
